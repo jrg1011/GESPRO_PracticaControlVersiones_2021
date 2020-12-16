6e355a895fa22f03eb1fc59734f9a9d807aeb04c
Jorge <jrg1011@alu.ubu.es>
2020-12-16 21:28:50 +0100

    Update build tools to 26.0.2 #7

---
+++ MERGE_CONFLICT.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Vamos a realizar cambios de forma simultánea a este fichero para simular con merge commit"
+echo "Intentaremos que dos miembros modifiquen la misma línea para crear la issue, partiendo desde esta versión del archivo"
+
+# What to backup. 
+backup_files="/home /var/spool/mail /etc /root /boot /opt"
+
+# Where to backup to.
+dest="/mnt/backup"
+
+# Create archive filename.
+day=$(date +%A)
+hostname=$(hostname -s)
+archive_file="$hostname-$day.tgz"
+
+# Print start status message.
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+echo
+
+# Backup the files using tar.
+tar czf $dest/$archive_file $backup_files
+
+# Print end status message.
+echo
+echo "Backup finished"
+date
+
+# Long listing of files in $dest to check file sizes.
+ls -lh $dest
